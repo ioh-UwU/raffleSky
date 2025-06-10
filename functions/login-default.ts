69f5670b7d93066848059472fd016b4330682438
ioh <81399391+ioh-UwU@users.noreply.github.com>
2025-06-09 19:04:56 -0500

    added backend CF function maybe?

---
+++ functions/login-default.ts
@@ -0,0 +1,22 @@
+import type { ExportedHandler } from "@cloudflare/workers-types";
+import { AtpAgent } from "@atproto/api";
+
+export interface Env {
+  DEFAULT_APP_PWD: string;
+}
+export default {
+  async fetch(request, env, ctx):Promise<Response> {
+    const clientIP = request.headers.get("CF-Connecting-IP");
+    const defaultUser = "raffle.iohtheprotogen.art";
+    const defaultPassword = env.DEFAULT_APP_PWD;
+    let agent = new AtpAgent({service: "https://bsky.social"});
+    let response:any = ''
+    try {
+        await agent.login({identifier: defaultUser, password: defaultPassword});
+        response = agent;
+    } catch {
+        response = null;
+    };
+    return new Response(JSON.stringify(response));
+  },
+} satisfies ExportedHandler<Env>;
