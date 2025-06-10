1b1e3911d7ad8b25caf46949ce05fcc9ff676715
ioh <81399391+ioh-UwU@users.noreply.github.com>
2025-06-09 19:43:02 -0500

    reconfigured dev environment a bit

--- functions/login-default.ts
+++ functions/login-default.ts
@@ -4,18 +4,16 @@ import { AtpAgent } from "@atproto/api";
 export interface Env {
   DEFAULT_APP_PWD: string;
 }
-export default {
-  async fetch(request, env, ctx):Promise<Response> {
-    const defaultUser = "raffle.iohtheprotogen.art";
-    const defaultPassword = env.DEFAULT_APP_PWD;
-    let agent = new AtpAgent({service: "https://bsky.social"});
-    let response:any = ''
-    try {
-        await agent.login({identifier: defaultUser, password: defaultPassword});
-        response = agent;
-    } catch {
-        response = null;
-    };
-    return new Response(JSON.stringify(response));
-  },
-} satisfies ExportedHandler<Env>;
+export const onRequestPost = async ({ request, env }) => {
+  const defaultUser = "raffle.iohtheprotogen.art";
+  const defaultPassword = env.DEFAULT_APP_PWD;
+  let agent = new AtpAgent({service: "https://bsky.social"});
+  let response:any = ''
+  try {
+      await agent.login({identifier: defaultUser, password: defaultPassword});
+      response = agent;
+  } catch {
+      response = null;
+  };
+  return new Response(JSON.stringify(response));
+};
