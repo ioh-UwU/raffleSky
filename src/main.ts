1de6caf509a85fc400ae4f563c8741c2a13b2ae5
ioh <81399391+ioh-UwU@users.noreply.github.com>
2025-06-12 19:07:53 -0500

    Formatting and style changes, added buttons for importing and exporting configurations as json (not yet implemented)

--- src/main.ts
+++ src/main.ts
@@ -5,40 +5,38 @@ function toggleElementVisibility(ids:string[]) {
     for (let id of ids) {
         let element = document.getElementById(id);
         element.className = element.className === "hide" ? "show" : "hide";
-    };
-};
-
+    }
+}
+let timerIDs = [];
 function fadeInElement(element:HTMLElement, msDuration:number) {
-    if (element.className === "hide") {
-        element.className = "show";
-        const step = 0.05; // 20 times
-        const delay = msDuration / 20;
-        let opacity = 0;
-        let timer = setInterval(() => {
-            opacity += step;
-            if (opacity > 1) {
-                opacity = 1;
-                clearInterval(timer);
-            };
-            element.style.opacity = opacity.toPrecision(2);
-        }, delay);
-    };
-};
+    element.className = "show";
+    const step = 0.05; // 20 times
+    const delay = msDuration / 20;
+    let opacity = 0;
+    let timer = setInterval(() => {
+        opacity += step;
+        if (opacity > 1) {
+            opacity = 1;
+            window.clearInterval(timer);
+        }
+        element.style.opacity = opacity.toPrecision(2);
+    }, delay);
+    timerIDs.push(timer);
+}
 function fadeOutElement(element:HTMLElement, msDuration:number) {
-    if (element.className === "show") { 
-        const step = 0.05; // 20 times
-        const delay = msDuration / 20;
-        let opacity = 1;
-        let timer = setInterval(() => {
-            opacity -= step;
-            if (opacity < 0) {
-                opacity = 0;
-                clearInterval(timer);
-            };
-            element.style.opacity = opacity.toPrecision(2);
-        }, delay);
-    };
-};
+    const step = 0.05; // 20 times
+    const delay = msDuration / 20;
+    let opacity = 1;
+    let timer = setInterval(() => {
+        opacity -= step;
+        if (opacity < 0) {
+            opacity = 0;
+            window.clearInterval(timer);
+        }
+        element.style.opacity = opacity.toPrecision(2);
+    }, delay);
+    timerIDs.push(timer);
+}
 
 function createFilterTag(inputText:HTMLInputElement, outputList:HTMLElement) {
     let newTagText = inputText.value.trim();
@@ -48,25 +46,25 @@ function createFilterTag(inputText:HTMLInputElement, outputList:HTMLElement) {
             if (newTagText === (<Element>tag).textContent) {
                 newTag = false;
                 break;
-            };
-        };
+            }
+        }
         if (newTag) {
             let newTagElement = document.createElement("button");
             newTagElement.textContent = newTagText;
             newTagElement.className = "tag";
             outputList.appendChild(newTagElement);
             newTagElement.addEventListener("click", () => {newTagElement.remove()});
-        };
-    };
+        }
+    }
     inputText.value = "";
-};
+}
 function getTags(tagList:HTMLInputElement) {
     let output = [];
     for (let tag of tagList.children) {
         output.push(tag.textContent);
-    };
+    }
     return output.length > 0 ? output : [""];
-};
+}
 function tagTextboxShortcuts(event:KeyboardEvent, inputText:HTMLInputElement, outputList:HTMLElement) {
     if (event.key === "Enter") {
         createFilterTag(inputText, outputList);
@@ -75,18 +73,40 @@ function tagTextboxShortcuts(event:KeyboardEvent, inputText:HTMLInputElement, ou
     } else if (event.key === "Delete") {
         while (outputList.children.length > 0) {
             outputList.children[0].remove();
-        };
-    };
-};
+        }
+    }
+}
+
+function showError(text:string) {
+    errorText.textContent = text;
+    for (let timer of timerIDs) {
+        window.clearInterval(timer);
+    }
+    fadeInElement(errorText, 500);
+    let errorTimeout = setTimeout(() => {
+        fadeOutElement(errorText, 1000);
+    }, 3000);
+    timerIDs.push(errorTimeout);
+}
 
 // Initialize page elements
+const importConfigButton = document.getElementById("import-config");
+importConfigButton.addEventListener("click", () => {
+    //TODO: Implement this.
+});
+
+const exportConfigButton = document.getElementById("export-config");
+exportConfigButton.addEventListener("click", () => {
+    //TODO: Implement this.
+});
+
 const linkInput = <HTMLInputElement>document.getElementById("link");
 
 const numWinnersInput = <HTMLInputElement>document.getElementById("winners");
 numWinnersInput.addEventListener("keyup", (event) => {
     if (event.key === "Escape") {
         numWinnersInput.value = "1";
-    };
+    }
 });
 const followCheckbox = <HTMLInputElement>document.getElementById("follow");
 const likeCheckbox = <HTMLInputElement>document.getElementById("like");
@@ -100,19 +120,19 @@ const imageEmbedCheckbox = <HTMLInputElement>document.getElementById("image-embe
 imageEmbedCheckbox.addEventListener("click", () => {
     if (imageEmbedCheckbox.checked) {
         anyEmbedCheckbox.checked = false;
-    };
+    }
 });
 const videoEmbedCheckbox = <HTMLInputElement>document.getElementById("video-embed");
 videoEmbedCheckbox.addEventListener("click", () => {
     if (videoEmbedCheckbox.checked) {
         anyEmbedCheckbox.checked = false;
-    };
+    }
 });
 const gifEmbedCheckbox = <HTMLInputElement>document.getElementById("gif-embed");
 gifEmbedCheckbox.addEventListener("click", () => {
     if (gifEmbedCheckbox.checked) {
         anyEmbedCheckbox.checked = false;
-    };
+    }
 });
 const anyEmbedCheckbox = <HTMLInputElement>document.getElementById("any-embed");
 anyEmbedCheckbox.addEventListener("click", () => {
@@ -120,7 +140,7 @@ anyEmbedCheckbox.addEventListener("click", () => {
         imageEmbedCheckbox.checked = false;
         videoEmbedCheckbox.checked = false;
         gifEmbedCheckbox.checked = false;
-    };
+    }
 });
 const hostReplyCheckbox = <HTMLInputElement>document.getElementById("host-reply");
 hostReplyCheckbox.checked = false;
@@ -135,7 +155,7 @@ const hostReplyInput = <HTMLInputElement>document.getElementById("reply-text");
 const hostReplyList = <HTMLInputElement>document.getElementById("reply-text-list");
 hostReplyInput.addEventListener("keyup", (event) => tagTextboxShortcuts(event, hostReplyInput, hostReplyList));
 
-const hostReplyAddButton = <HTMLInputElement>document.getElementById("add-reply-button");
+const hostReplyAddButton = document.getElementById("add-reply-button");
 hostReplyAddButton.addEventListener("click", () => {createFilterTag(hostReplyInput, hostReplyList)});
 
 const replyCaseSensitiveCheckbox = <HTMLInputElement>document.getElementById("case-sensitive");
@@ -149,11 +169,13 @@ const userFilterInput = <HTMLInputElement>document.getElementById("user-filter-t
 const userFilterList = <HTMLInputElement>document.getElementById("user-filter-list");
 userFilterInput.addEventListener("keyup", (event) => tagTextboxShortcuts(event, userFilterInput, userFilterList));
 
-const userFilterAddButton = <HTMLInputElement>document.getElementById("add-user-filter-button");
+const userFilterAddButton = document.getElementById("add-user-filter-button");
 userFilterAddButton.addEventListener("click", () => {createFilterTag(userFilterInput, userFilterList)});
 
-const raffleButton = <HTMLInputElement>document.getElementById("run-raffle");
+const raffleButton = document.getElementById("run-raffle");
 raffleButton.addEventListener("click", () => runRaffle())
+
+const errorText = document.getElementById("error");
 
 var rerollCandidates = [];
 const winnerSection = document.getElementById("winner-section");
@@ -162,16 +184,6 @@ clearWinnersButton.addEventListener("click", () => clearWinners());
 const rerollButton = document.getElementById("reroll");
 rerollButton.addEventListener("click", () => rerollWinners());
 const displayWinners = document.getElementById("winner-grid");
-
-const errorText = document.getElementById("error");
-
-function showError(text:string) {
-    errorText.textContent = text;
-    fadeInElement(errorText, 500);
-    setTimeout(() => {
-        fadeOutElement(errorText, 1000);
-    }, 3000);
-};
 
 // Config
 function setRaffleConfig() {
@@ -198,15 +210,15 @@ function setRaffleConfig() {
         },
         blockList: userFilterCheckbox.checked,
         blockedHandles: getTags(userFilterList)
-    };
+    }
     return raffleConfig;
-};
+}
 
 // API calls
 async function signIn() {
     let agent = new AtpAgent({service: "https://public.api.bsky.app"});
     return agent;
-};
+}
 
 // Raffle data requests
 async function getHostInfo(agent:AtpAgent, link:string) {
@@ -226,8 +238,8 @@ async function getHostInfo(agent:AtpAgent, link:string) {
         hostHandle: handle, 
         hostAvatar: avatar, 
         hostDisplayName: displayName
-    };
-};
+    }
+}
 
 async function getFollows(agent:AtpAgent, hostActor:string) {
     let output = [];
@@ -236,9 +248,9 @@ async function getFollows(agent:AtpAgent, hostActor:string) {
         let returnData = (await agent.app.bsky.graph.getFollowers({actor: hostActor, limit: 100, cursor: enumCursor})).data;
         enumCursor = returnData.cursor;
         output = output.concat(returnData.followers);
-    };
+    }
     return output;
-};
+}
 
 async function getLikes(agent:AtpAgent, postUri:string) {
     let likes = [];
@@ -247,13 +259,13 @@ async function getLikes(agent:AtpAgent, postUri:string) {
         let returnData = (await agent.app.bsky.feed.getLikes({uri: postUri, limit: 100, cursor: enumCursor})).data;
         enumCursor = returnData.cursor;
         likes = likes.concat(returnData.likes);
-    };
+    }
     let output = [];
     for (let like of likes) {
         output.push(like["actor"]);
-    };
+    }
     return output;
-};
+}
 
 async function getReposts(agent:AtpAgent, postUri:string) {
     let output = [];
@@ -262,13 +274,13 @@ async function getReposts(agent:AtpAgent, postUri:string) {
         let returnData = (await agent.app.bsky.feed.getRepostedBy({uri: postUri, limit: 100, cursor: enumCursor})).data;
         enumCursor = returnData.cursor;
         output = output.concat(returnData.repostedBy);
-    };
+    }
     return output;
-};
+}
 
 async function getComments(agent:AtpAgent, postUri:string) {
     return (await agent.app.bsky.feed.getPostThread({uri: postUri, depth: 2})).data.thread["replies"];
-};
+}
 
 function commentEmbedFilter(comments:Object, embedTypes:Object) {
     let output = [];
@@ -278,29 +290,29 @@ function commentEmbedFilter(comments:Object, embedTypes:Object) {
             if (embedTypes["any"] && embedData !== undefined){
                 output.push(comment);
                 continue; 
-            };
+            }
             try { // Check embed for image.
                 if (embedTypes["image"] && embedData["$type"].includes("image")) {
                     output.push(comment);
                     continue; 
-                };
-            } catch {};
+                }
+            } catch {}
             try { // Check embed for video.
                 if (embedTypes["video"] && embedData["$type"].includes("video")) {
                     output.push(comment);
                     continue; 
-                };
-            } catch {};
+                }
+            } catch {}
             try { // Check embed for GIF.
                 if (embedTypes["gif"] && embedData["external"]["uri"].includes("://media.tenor.com/") && embedData["external"]["uri"].includes(".gif")) {
                     output.push(comment);
                     continue; 
-                };
-            } catch {};
-        } catch {};
-    };
+                }
+            } catch {}
+        } catch {}
+    }
     return output;
-};
+}
 function commentReplyFilter(comments:Object, hostHandle:string, replyConfig:Object) {
     let output = [];
     for (let [_, comment] of Object.entries(comments)) {
@@ -317,31 +329,33 @@ function commentReplyFilter(comments:Object, hostHandle:string, replyConfig:Obje
                             if (!replyConfig["caseSensitive"]) {
                                 validReply = validReply.toLocaleLowerCase();
                                 contents = contents.toLocaleLowerCase();
-                            };
+                            }
                             isValidReply = replyConfig["exact"] ? isValidReply = contents === validReply : contents.includes(validReply); 
-                            if (isValidReply) {break};
-                        };
+                            if (isValidReply) {
+                                break;
+                            }
+                        }
                         if (isValidReply) {
                             output.push(comment);
                             continue;
-                        };
+                        }
                     } else {
                         output.push(comment);
                         continue;
-                    };
-                };
-            };
-        };
-    };
+                    }
+                }
+            }
+        }
+    }
     return output;
-};
+}
 function getCommentActors(comments:Object) {
     let output = [];
     for (let [_, comment] of Object.entries(comments)) {
         output.push(comment["post"]["author"]);
-    };
+    }
     return output;
-};
+}
 
 function getCandidates(candidateGroups:Array<Array<Object>>, denyList:Array<string>) {
     let prevBuffer = [];
@@ -354,27 +368,27 @@ function getCandidates(candidateGroups:Array<Array<Object>>, denyList:Array<stri
                 firstGroup = false;
                 prevBuffer = group;
                 continue;
-            };
+            }
             for (let [_, candidate] of Object.entries(group)) {
                 for (let prev of prevBuffer) {
                     if (candidate["handle"] === prev["handle"]) {
                         output.push(candidate);
                         break;
-                    };
-                };
-            };
+                    }
+                }
+            }
             prevBuffer = output;
             output = [];
-        };
-    };
+        }
+    }
     // Apply user filter.
     for (let [_, candidate] of Object.entries(prevBuffer)) {
         if (!denyList.includes(candidate["handle"])) {
             output.push(candidate);
-        };
-    };
+        }
+    }
     return output;
-};
+}
 
 function pickWinners(candidates:Array<Object>, numWinners:number, reroll?:boolean) {
     if (!reroll) {
@@ -385,10 +399,10 @@ function pickWinners(candidates:Array<Object>, numWinners:number, reroll?:boolea
         let winnerIndex = Math.floor(Math.random() * candidates.length);
         winners.push(candidates.splice(winnerIndex, 1));
         winners = winners.flat();
-    };
+    }
     rerollCandidates = candidates;
     return winners;
-};
+}
 
 function addWinner(winner:Object) {
     let handle = winner["handle"];
@@ -397,7 +411,7 @@ function addWinner(winner:Object) {
     let name = winner["displayName"] != "" ? winner["displayName"] : handle;
     if (name.length > 16) {
         name = name.substring(0, 16) + "...";
-    };
+    }
 
     let winnerSpan = document.createElement("span");
     winnerSpan.id = handle;
@@ -434,15 +448,15 @@ function addWinner(winner:Object) {
 
     winnerSpan.addEventListener("click", () => toggleReroll(infoDiv.id));
     return winnerSpan;
-};
+}
 
 function clearWinners() {
     while (displayWinners.children.length > 0) {
         displayWinners.children[0].remove();
-    };
+    }
     winnerSection.className = "hide";
     displayWinners.className = "hide";
-};
+}
 
 function toggleReroll(targetId:string) {
     let selection = document.getElementById(targetId);
@@ -454,20 +468,20 @@ function toggleReroll(targetId:string) {
         selection.className = "winner-info-reroll-select-again";
     } else if ((selection.className === "winner-info-reroll-select-again")) {
         selection.className = "winner-info-rerolled";
-    };
+    }
     rerollButton.className = "hide";
     for (let element of displayWinners.children) {
         for (let child of element.children) {
             if (["winner-info-reroll-select", "winner-info-reroll-select-again"].includes(child.className)) {
                 rerollButton.className = "show";
                 break;
-            };
-        };
+            }
+        }
         if (rerollButton.className === "show") {
             break;
-        };
-    };
-};
+        }
+    }
+}
 
 function rerollWinners() {
     for (let oldWinner of displayWinners.children) {
@@ -478,62 +492,66 @@ function rerollWinners() {
                     let newWinnerElement = addWinner(newWinner);
                     displayWinners.replaceChild(newWinnerElement, oldWinner);
                     document.getElementById(`${newWinnerElement.id}-info`).className = "winner-info-rerolled";
-                };
-            };
-        };
-    };
-};
+                }
+            }
+        }
+    }
+}
 
 // Raffle procedure
 async function runRaffle() {
     showError("Please wait...")
     clearWinners();
     let raffleConfig = setRaffleConfig();
-    let agent = await signIn();
+    if (raffleConfig.link === "") {
+        showError("No post specifiied.");
+        return;
+    }
     if ([raffleConfig.follow, raffleConfig.like, raffleConfig.repost, raffleConfig.comment].find((a) => {return a === false})) {
         showError("No raffle options set!");
         return;
-    };
+    }
+    let agent = await signIn();
     let postInfo = await getHostInfo(agent, raffleConfig.link);
 
     // Ensure raffle host can't win.
     if (!raffleConfig.blockedHandles.includes(postInfo.hostHandle)) {
         raffleConfig.blockedHandles.push(postInfo.hostHandle);
-    };
+    }
     raffleConfig.blockedHandles = raffleConfig.blockedHandles.filter((a) => {return a != ""});
     if (raffleConfig.follow) {
         var follows = await getFollows(agent, postInfo.hostHandle);
-    };
+    }
     if (raffleConfig.like) {
         var likes = await getLikes(agent, postInfo.postUri);
-    };
+    }
     if (raffleConfig.repost) {
         var reposts = await getReposts(agent, postInfo.postUri);
-    };
+    }
     if (raffleConfig.comment) {
         var comments = await getComments(agent, postInfo.postUri);
         if (Object.values(raffleConfig.embedTypes).find((a) => {return a === true})) {
             comments = commentEmbedFilter(comments, raffleConfig.embedTypes);
-        };
+        }
         if (raffleConfig.hostReply.enabled) {
             comments = commentReplyFilter(
                 comments,
                 postInfo.hostHandle,
                 raffleConfig.hostReply
-            );
-        };
+            )
+        }
         comments = getCommentActors(comments);
-    };
+    }
     let candidates = getCandidates([follows, likes, reposts, comments], raffleConfig.blockedHandles);
     if (candidates.length > 0) {
         var winners = candidates.length > raffleConfig.winners ? pickWinners(candidates, raffleConfig.winners) : candidates;
     } else {
         showError("No viable candidates. No winners!");
         return;
-    };
+    }
     for (let winner of winners) {
         displayWinners.appendChild(addWinner(winner));
-    };
+    }
     winnerSection.className = "show";
     displayWinners.className = "show";
 
@@ -545,4 +563,4 @@ async function runRaffle() {
         behavior: "smooth",
         block: "start"
     });
-};
+}
